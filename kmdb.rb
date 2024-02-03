d27817f3dbf28e8d0d5212ad1480e2c22fada6f9
suevin choi <suevin.choi@kellogg.northwestern.edu>
2024-02-03 23:30:53 +0000

    1st draft

--- kmdb.rb
+++ kmdb.rb
@@ -78,6 +78,203 @@
 # Do not use hard-coded foreign key IDs.
 # TODO!
 
+
+### Table for Movie + Insert data  
+Movie.destroy_all 
+
+movie = Movie.new 
+movie["title"] = "Batman Begins" 
+movie["year_released"] = "2005" 
+movie["rated"] = "PG-13" 
+movie["studio_id"] = "1" 
+movie.save 
+
+movie = Movie.new 
+movie["title"] = "The Dark Knight" 
+movie["year_released"] = "2008" 
+movie["rated"] = "PG-13" 
+movie["studio_id"] = "1" 
+movie.save 
+
+movie = Movie.new 
+movie["title"] = "The Dark Knight Rises" 
+movie["year_released"] = "2012" 
+movie["rated"] = "PG-13" 
+movie["studio_id"] = "1" 
+movie.save 
+
+# puts movie.inspect 
+
+
+
+### Table for Studio + Insert data  
+
+Studio.destroy_all 
+
+studio = Studio.new 
+studio["name"] = "Warner Bros." 
+studio.save 
+
+# puts studio.inspect 
+
+
+
+### Table for Actor + Insert data 
+
+Actor.destroy_all 
+
+actor = Actor.new 
+actor["name"] = "Christian Bale" #1 
+actor.save 
+
+actor = Actor.new 
+actor["name"] = "Michael Caine" #2 
+actor.save 
+
+actor = Actor.new 
+actor["name"] = "Liam Neeson" #3 
+actor.save 
+
+actor = Actor.new 
+actor["name"] = "Katie Holmes" #4 
+actor.save 
+
+actor = Actor.new 
+actor["name"] = "Gary Oldman" #5 
+actor.save 
+
+actor = Actor.new 
+actor["name"] = "Heath Ledger" #6 
+actor.save 
+
+actor = Actor.new 
+actor["name"] = "Aaron Eckhart" #7 
+actor.save 
+
+actor = Actor.new 
+actor["name"] = "Maggie Gyllenhaal" #8 
+actor.save 
+
+actor = Actor.new 
+actor["name"] = "Tom Hardy" #9 
+actor.save 
+
+actor = Actor.new 
+actor["name"] = "Joseph Gordon-Levitt" #10 
+actor.save 
+
+actor = Actor.new 
+actor["name"] = "Anne Hathaway" #11 
+actor.save 
+
+# puts actor.inspect 
+
+
+
+### Table for Role + Insert data 
+
+Role.destroy_all 
+
+
+### Batman Begins 
+role = Role.new 
+role["character_name"] = "Bruce Wayne" 
+role["movie_id"] = "1" 
+role["actor_id"] = "1" 
+role.save 
+
+
+role = Role.new 
+role["character_name"] = "Alfred" 
+role["movie_id"] = "1" 
+role["actor_id"] = "2" 
+role.save 
+
+role = Role.new 
+role["character_name"] = "Ra's Al Ghul" 
+role["movie_id"] = "1" 
+role["actor_id"] = "3" 
+role.save 
+
+role = Role.new 
+role["character_name"] = "Rachel Dawes" 
+role["movie_id"] = "1" 
+role["actor_id"] = "4" 
+role.save 
+
+role = Role.new 
+role["character_name"] = "Commissioner Gordon" 
+role["movie_id"] = "1" 
+role["actor_id"] = "5" 
+role.save 
+
+
+### The Dark Knight 
+role = Role.new 
+role["character_name"] = "Bruce Wayne" 
+role["movie_id"] = "2" 
+role["actor_id"] = "1" 
+role.save 
+
+role = Role.new 
+role["character_name"] = "Joker" 
+role["movie_id"] = "2" 
+role["actor_id"] = "6" 
+
+role.save 
+role = Role.new 
+role["character_name"] = "Harvey Dent" 
+role["movie_id"] = "2" 
+role["actor_id"] = "7" 
+role.save 
+
+role = Role.new 
+role["character_name"] = "Alfred" 
+role["movie_id"] = "2" 
+role["actor_id"] = "2" 
+role.save 
+
+role = Role.new 
+role["character_name"] = "Rachel Dawes" 
+role["movie_id"] = "2" 
+role["actor_id"] = "8" 
+role.save 
+
+
+### The Dark Knight Rises 
+role = Role.new 
+role["character_name"] = "Bruce Wayne" 
+role["movie_id"] = "3" 
+role["actor_id"] = "1" 
+role.save 
+
+role = Role.new 
+role["character_name"] = "Commissioner Gordon" 
+role["movie_id"] = "3" 
+role["actor_id"] = "5" 
+role.save 
+
+role = Role.new 
+role["character_name"] = "Bane" 
+role["movie_id"] = "3" 
+role["actor_id"] = "9" 
+role.save 
+
+role = Role.new 
+role["character_name"] = "John Blake" 
+role["movie_id"] = "3" 
+role["actor_id"] = "10" 
+role.save 
+
+role = Role.new 
+role["character_name"] = "Selina Kyle" 
+role["movie_id"] = "3" 
+role["actor_id"] = "11" 
+role.save 
+
+puts role.inspect 
+
+
 # Prints a header for the movies output
 puts "Movies"
 puts "======"
